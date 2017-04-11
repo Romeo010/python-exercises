047eff4e15d0d181ec6db0a0ddb7442f542166a2
Romeo010 <romeo.ekirapa@outlook.com>
2017-04-11 16:18:04 +0300

    Create homework_1.py

---
+++ Homework 1/homework_1.py
@@ -0,0 +1,64 @@
+# Name: Romeo Ekirapa
+# Date: Mar 31, 2017
+# homework_1.py
+
+##### Template for Homework 1, exercises 1.2-1.4 ######
+
+print "********** Exercise 1.2 **********"
+
+print  " | |"
+print "-----"
+print  " | |"
+print "-----"
+print  " | |"
+
+print "********** Exercise 1.3 **********"
+
+a = " | |"
+b = "-----"
+
+print a
+print b
+print a
+print b
+print a
+
+print "********** Exercise 1.4 **********"
+
+first_name = raw_input("Enter your first name: ")
+last_name = raw_input("Enter your last name: ")
+print "Enter your date of birth: "
+month = raw_input("Month? ")
+day = raw_input("Day? ")
+year = raw_input("Year? ")
+print first_name+(" ")+last_name+(" was born on ")+month+(" ")+day+(", ")+year+(".")
+
+
+
+################ Results ################
+
+Python 2.7.3 (default, Apr 10 2012, 23:31:26) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+********** Exercise 1.2 **********
+ | |
+-----
+ | |
+-----
+ | |
+********** Exercise 1.3 **********
+ | |
+-----
+ | |
+-----
+ | |
+********** Exercise 1.4 **********
+Enter your first name: Romeo
+Enter your last name: Ekirapa
+Enter your date of birth: 
+Month? September
+Day? 13
+Year? 1994
+Romeo Ekirapa was born on September 13, 1994.
+>>> 
